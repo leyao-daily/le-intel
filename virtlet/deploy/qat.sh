af5a7b8acb59e8ec7128ed59a6b2fc90a695eaab
Yao Le <le.yao@intel.com>
2019-12-19 00:48:09 -0500

    dpdk and vpp install

--- virtlet/deploy/qat.sh
+++ virtlet/deploy/qat.sh
@@ -10,6 +10,7 @@ cp /usr/bin/python3 /usr/bin/python
 apt install -y vpp
 apt install -y vpp-plugin-dpdk
 apt install -y make gcc libnuma-dev
+apt-get install libssl-dev
 
 cd /opt
 git clone http://dpdk.org/git/dpdk
@@ -18,3 +19,9 @@ export RTE_TARGET=x86_64-native-linuxapp-gcc/
 export DESTDIR=/opt/dpdk
 export RTE_SDK=/opt/dpdk
 make install T=x86_64-native-linux-gcc
+
+modprobe uio
+
+insmod /opt/dpdk/x86_64-native-linux-gcc/kmod/igb_uio.ko
+
+python3 /opt/dpdk/usertools/dpdk-devbind.py -s
