6e7cde51348f03410935baf3846441efca864a8e
Le Yao <le.yao@intel.com>
2022-03-21 10:55:28 +0800

    Update setup_k8s.sh
    Signed-off-by: Le Yao <le.yao@intel.com>

--- scripts/setup_k8s.sh
+++ scripts/setup_k8s.sh
@@ -28,7 +28,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 
 ##set up the stable repository
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 ##Installation
