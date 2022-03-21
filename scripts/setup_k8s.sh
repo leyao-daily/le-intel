a0e1f199253dadc1d604cd75adaf2b3bcc5104ab
Le Yao <le.yao@intel.com>
2022-03-21 10:33:28 +0800

    Update setup_k8s.sh
    Signed-off-by: Le Yao <le.yao@intel.com>

--- scripts/setup_k8s.sh
+++ scripts/setup_k8s.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #You can define the Kubernetes Version here
-VERSION=1.23.2
+VERSION=1.23.4
 
 #Prepare
 swapoff -a
@@ -24,13 +24,12 @@ sudo apt-get install -y \
     software-properties-common
 
 ##add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 ##set up the stable repository
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 ##Installation
 sudo apt-get update
